8ac7ec2f986b6937dde7c2623e4e1daf810b358d
poniitty <pekka.niittynen@helsinki.fi>
2021-11-08 15:28:15 +0100

    Updating enabled and station summary

--- scripts/Get_FMI_data.R
+++ scripts/Get_FMI_data.R
@@ -28,18 +28,30 @@ plot(st_geometry(selected_stations))
 
 # Daily
 
-startdate <- "1990-01-01"
+# Empty dataframe or previous data
+if(file.exists("output/fmi_daily.csv")){
+  df <- as_tibble(read.csv("output/fmi_daily.csv")) %>% 
+    mutate(time = as_date(time))
+} else {
+  df <- data.frame()
+}
+
+if(file.exists("output/fmi_daily.csv")){
+  startdate <- max(df$time)
+} else {
+  startdate <- "1990-03-01"
+}
 enddate <- Sys.Date()
 
-days <- seq(as_date(startdate), as_date(enddate), by  = "month")
+days <- seq(as_date(floor_date(as_date(startdate)-months(2))), as_date(enddate), by  = "month")
 
-df <- data.frame()
+
 for(i in 1:NROW(selected_stations)){
-  # i <- 9
+  # i <- 2
   print(selected_stations$Name[i])
   
   for(ii in days){
-    
+    # ii <- "2021-08-21"
     print(as_date(ii))
     
     tryCatch({
@@ -47,7 +59,7 @@ for(i in 1:NROW(selected_stations)){
                                    endtime = (as_date(ii) + months(1)) - 1,
                                    fmisid = selected_stations$FMISID[i])
       d %>% st_drop_geometry() %>% 
-        #filter(complete.cases(.)) %>% 
+        filter(complete.cases(.)) %>% 
         pivot_wider(names_from = "variable", values_from = "value") %>% 
         mutate(FMISID = selected_stations$FMISID[i]) -> d
       
@@ -57,16 +69,28 @@ for(i in 1:NROW(selected_stations)){
   }
 }
 
+df %>% filter(!duplicated(df %>% select(time, FMISID), fromLast = T)) -> df
+
 write_csv(df, "output/fmi_daily.csv")
 
 # HOURLY
 
-startdate <- "2018-01-01"
+# Empty dataframe or previous data
+if(file.exists("output/fmi_hourly.csv")){
+  df <- as_tibble(read.csv("output/fmi_hourly.csv"))
+} else {
+  df <- data.frame()
+}
+
+if(file.exists("output/fmi_hourly.csv")){
+  startdate <- max(df$time)
+} else {
+  startdate <- "2018-03-01"
+}
 enddate <- Sys.Date()
 
-days <- seq(as_date(startdate), as_date(enddate), by  = "month")
+days <- seq(as_date(floor_date(as_date(startdate)-months(2))), as_date(enddate), by  = "month")
 
-df <- data.frame()
 for(i in 1:NROW(selected_stations)){
   # i <- 1
   print(selected_stations$Name[i])
@@ -82,15 +106,60 @@ for(i in 1:NROW(selected_stations)){
       d %>% st_drop_geometry() %>% 
         filter(complete.cases(.)) %>% 
         pivot_wider(names_from = "variable", values_from = "value") %>% 
-        mutate(area = selected_stations$area[i],
-               FMISID = selected_stations$FMISID[i]) -> d
+        mutate(FMISID = selected_stations$FMISID[i]) -> d
       
       df <- bind_rows(df, d)
     }, error = function(e) { print("NO DATA FOR THESE DATES") })
   }
 }
 
+df %>% filter(!duplicated(df %>% select(time, FMISID), fromLast = T)) -> df
+
 write_csv(df, "output/fmi_hourly.csv")
 
+# Station summary
+
+selected_stations
+
+df <- as_tibble(read.csv("output/fmi_daily.csv")) %>% 
+  mutate(time = as_date(time))
+
+df %>% group_by(FMISID) %>% 
+  summarise(first_date = min(time),
+            last_date = max(time),
+            tdays = sum(!is.na(tday)),
+            rrdays = sum(!is.na(rrday))) -> aggr
+
+selected_stations <- full_join(selected_stations, aggr)
+
+bind_cols(p, p %>% st_coordinates() %>% as.data.frame()) %>% 
+  st_drop_geometry() %>% 
+  group_by(area) %>% 
+  summarise(X = mean(X),
+            Y = mean(Y)) %>% 
+  st_as_sf(., coords = c("X","Y"), crs = st_crs(p)) -> centroids
 
 
+selected_stations %>% 
+  mutate(KAR = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "KAR"))/1000),1),
+         HYY = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "HYY"))/1000),1),
+         PIS = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "PIS"))/1000),1),
+         TII = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "TII"))/1000),1),
+         VAR = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "VAR"))/1000),1),
+         AIL = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "AIL"))/1000),1),
+         SAA = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "SAA"))/1000),1),
+         MAL = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "MAL"))/1000),1),
+         RAS = round(as.numeric(st_distance(selected_stations, centroids %>% filter(area == "RAS"))/1000),1)) -> selected_stations
+
+
+selected_stations$closest_area <- names(selected_stations %>% select(KAR:RAS) %>% st_drop_geometry())[apply(selected_stations %>% select(KAR:RAS) %>% st_drop_geometry(), 1, function(x) which.min(x))]
+
+bind_cols(selected_stations, selected_stations %>% st_coordinates() %>% as.data.frame()) %>% 
+  st_drop_geometry() %>% 
+  select(-Groups,-within50km) %>% 
+  relocate(X, Y, .after = Elevation) %>% 
+  relocate(closest_area, .after = rrdays) %>% 
+  rename(x_utm35 = X,
+         y_utm35 = Y) -> selected_stations_df
+
+write_csv(selected_stations_df, "output/fmi_stations.csv")
